0c5cffa8b7ad76e3d8678de82c7a46bdd7cfd9ca
unknown <t.mousnia@gmail.com>
2022-04-29 10:53:16 +0000

    end section 8

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+import { IPagination } from './models/IPagination';
+import { IProduct } from './models/product';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit{
+  products:IProduct[];
+  title=' Skinet';
+
+  constructor(private httpClient:HttpClient) {
+    
+  }
+
+  ngOnInit(): void {
+    this.httpClient.get<IPagination>('https://localhost:5001/api/products?pagesize=50').subscribe((res:IPagination)=>{
+      this.products=res.data;
+    },error=>{
+      console.log(error);
+      
+    });
+  }
 }
