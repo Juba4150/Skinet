6d3e0aa423d8275941fa4e094f02668a9f8d14ff
unknown <t.mousnia@gmail.com>
2022-05-30 14:25:06 +0100

    end-section-09

---
+++ client/src/app/shared/components/pager/pager.component.ts
@@ -0,0 +1,24 @@
+import { Output } from '@angular/core';
+import { Input } from '@angular/core';
+import { Component, OnInit, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-pager',
+  templateUrl: './pager.component.html',
+  styleUrls: ['./pager.component.scss']
+})
+export class PagerComponent implements OnInit {
+  @Input() totalCount: number;
+  @Input() pageSize: number;
+  @Output() pageChanged = new EventEmitter<number>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+  onPagerChange(event: any) {
+    this.pageChanged.emit(event.page)
+  }
+}
